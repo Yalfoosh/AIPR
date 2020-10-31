b6a1893160f63fb1992d0bd335daf4e4409b8bda
Yalfoosh <headsouldev@gmail.com>
2020-10-31 20:09:55 +0100

    Dodaj licence u DZ-02

--- dz/dz-02/src/utils/function.py
+++ dz/dz-02/src/utils/function.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Yalfoosh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Callable, Union
 
 import numpy as np
@@ -13,13 +27,3 @@ def get_f3(starting_number: Union[float, int] = 1):
     return lambda x: np.sum(
         [np.square(x[i] - (starting_number + i)) for i in range(len(x))]
     )
-
-
-def get_f3_scalar(starting_number: Union[float, int] = 1):
-    if not isinstance(starting_number, (float, int)):
-        raise TypeError(
-            "Expected argument starting_number to be a float or int, instead it is "
-            f"{type(starting_number)}."
-        )
-
-    return lambda x: (x - starting_number) ** 2
