5096b28c90bc1b0f9dc83882424b60c956119214
Yalfoosh <headsouldev@gmail.com>
2020-10-31 20:33:58 +0100

    Dokumentiraj function.py

--- dz/dz-02/src/searches/function.py
+++ dz/dz-02/src/searches/function.py
@@ -13,11 +13,24 @@
 # limitations under the License.
 
 import copy
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 
 
 class Function:
+    """
+    A class wrapper for callable objects, which can count calls.
+    """
+
     def __init__(self, function_object: Callable):
+        """
+        The Function constructor.
+
+        Args:
+            function_object (Callable): A callable object representing some function.
+
+        Raises:
+            ValueError: Raised if argument function_object isn't callable.
+        """
         if not callable(function_object):
             raise ValueError("Argument function_object must be callable!")
 
@@ -26,25 +39,60 @@ class Function:
 
     @property
     def function_object(self) -> Callable:
+        """
+        The function_object property.
+
+        Returns:
+            Callable: A deep copy of the internal function object.
+        """
         return copy.deepcopy(self._function_object)
 
     @property
     def call_count(self) -> int:
+        """
+        The call_count property.
+
+        Returns:
+            int: An int representing the number of times the internal function object
+            has been called (excluding the times it was called with dont_count set to
+            True).
+        """
         return self._call_count
 
     @property
     def n(self) -> int:
+        """
+        An alias for the call_count property.
+
+        Returns:
+            int: Whatever call_count returns.
+        """
         return self.call_count
 
-    def call(self, *args, **kwargs):
+    def call(self, *args, **kwargs) -> Any:
+        """
+        Calls itself.
+
+        Returns:
+            Any: Whatever calling itself returns.
+        """
         return self(*args, **kwargs)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kwargs) -> Any:
+        """
+        Calls the function_object within with args and kwargs, and if dont_count isn't
+        set to true counts the function call.
+
+        Returns:
+            Any: Whatever the internal function object returns.
+        """
         dont_count = kwargs.get("dont_count", None)
 
         if dont_count is None:
             dont_count = False
         else:
+            # Do this so the actual function call isn't littered
+            # by the dont_count kwarg.
             kwargs.pop("dont_count")
 
         call_result = self._function_object(*args, **kwargs)
@@ -55,6 +103,18 @@ class Function:
         return call_result
 
     def reset(self, function_object: Optional[Callable] = None):
+        """
+        Resets the internal function object call counter to 0 and changes the internal
+        function object to a new one, if provided.
+
+        Args:
+            function_object (Optional[Callable], optional): A callable object you wish
+            to replace the internal function object with. Defaults to None (no change
+            of the internal function object).
+
+        Raises:
+            ValueError: Raised if a function object was passed, but it isn't callable.
+        """
         if function_object is not None:
             if not callable(function_object):
                 raise ValueError("Argument function_object must be callable!")
@@ -64,9 +124,25 @@ class Function:
         self._call_count = 0
 
     def get_deepcopy(self) -> "Function":
+        """
+        Returns the deep copy of this class instance.
+
+        Returns:
+            Function: A Function representing the deep copy of
+            this class instance.
+        """
         return copy.deepcopy(self)
 
     def get_new(self) -> "Function":
+        """
+        Returns a deep copy of itself with the function object
+        call counter reset to 0.
+
+        Returns:
+            Function: A Function representing the deep copy of
+            this class instance with the function object call
+            counter reset to 0.
+        """
         to_return = self.get_deepcopy()
         to_return.reset()
 
